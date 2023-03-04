b4442857dd2830b6695a827ef10b9e5a86b5275f
IvanNavin777 <iholovko@leviathansecurity.com>
2023-03-04 09:11:57 +0200

    makeToday

--- src/components/TimeTable/TimeTable.tsx
+++ src/components/TimeTable/TimeTable.tsx
@@ -7,12 +7,57 @@ import { ViewMode } from '../../types/types'
 
 import s from './TimeTable.module.sass'
 
+const makeToday = (str: string) => {
+  const arr = str.split(' ')
+  const halfCell = 18
+  const topOffset = 80
+  let x = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].startsWith('x=')) {
+      arr[i] = arr[i].substring(0, arr[i].length - 1)
+      arr[i] = arr[i].substring(3, arr[i].length)
+      x = +arr[i] + halfCell
+      arr[i] = `x="${Number(arr[i]) + halfCell}"`
+    } else if (arr[i].startsWith('y=')) {
+      arr[i] = `y="${topOffset}"`
+    } else if (arr[i].startsWith('height=')) {
+      arr[i] = arr[i].substring(0, arr[i].length - 1)
+      arr[i] = arr[i].substring(8, arr[i].length)
+      arr[i] = `height="${Number(arr[i]) - topOffset}"`
+    } else if (arr[i].startsWith('class=')) {
+      arr[i] = 'fill="black"'
+    } else if (arr[i].startsWith('width')) {
+      arr[i] = `width="2"`
+    }
+  }
+
+  const date = new Date().toLocaleDateString('en-US')
+  const text = `<text x="${x - 24}" y="${topOffset - 5}" fill="black" >${date}</text>`
+
+  return text + arr.join(' ') + '></rect>'
+}
+
 export const TimeTable = () => {
   const [viewMode, setViewMode] = useState(ViewMode.Month)
   const { data, dateType } = useTypedSelector((state) => state.gridReducer)
 
   useEffect(() => {
+    const vm = Object.values(dateType)[0]
+
     setViewMode(Object.values(dateType)[0])
+
+    if (vm === ViewMode.Day) {
+      setTimeout(() => {
+        const todayColumn = document.querySelector('.today-highlight')
+        const gantt = document.querySelector('.gantt')
+
+        if (gantt && todayColumn) {
+          const line = makeToday(todayColumn.outerHTML)
+          gantt.insertAdjacentHTML('beforeend', line)
+        }
+      })
+    }
   }, [dateType])
 
   return (
