2e417c9dca8ce9188bf8317550cf9aaf1de77841
IvanNavin777 <iholovko@leviathansecurity.com>
2023-03-05 19:00:02 +0200

    little fix

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,23 +1,19 @@
-import { shallowEqual } from 'react-redux';
+import { shallowEqual } from 'react-redux'
 
-import { useTypedSelector } from '../../store/store';
-import { DateSelect } from '../DateSelect';
+import { useTypedSelector } from '../../store/store'
+import { DateSelect } from '../DateSelect'
 
-import { Task } from './Task';
+import { Task } from './Task'
 
 import s from './Tasks.module.sass'
 
 export const Tasks = () => {
-  const {
-    data,
-  } = useTypedSelector((state) => state.gridReducer, shallowEqual)
+  const { data } = useTypedSelector((state) => state.gridReducer, shallowEqual)
 
   return (
     <div className={s.Tasks}>
       <DateSelect />
-      {data.map(({name}) => (
-        <Task taskName={name} key={name} />
-      ))}
+      {data.map(({ name }) => name && <Task taskName={name} key={name} />)}
     </div>
-  );
-};
+  )
+}
